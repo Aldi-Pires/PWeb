d9963677375077f3b5739c7491bfb690a98fd441
Aldana Pires <aldana.pires@gmail.com>
2022-05-10 23:54:28 -0300

    Ejercicio2

--- Mod1Uni4/Ejercicio2.js
+++ Mod1Uni4/Ejercicio2.js
@@ -5,11 +5,11 @@ var highestN = 0;
 for (i = 0; i <= 4; i++) {
     num[i] = prompt('Ingrese un número');
     num[i] = parseInt(num[i]);
-    
+
     if (num[i] > highestN) {
         highestN = num[i]
     }
 } 
-document.write(highestN);
+document.write('El valor más alto es ' + highestN);
 
 
