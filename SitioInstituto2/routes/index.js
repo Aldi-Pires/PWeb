c3be9cbac5ae8dae6037d7741ffa8338e9382470
Aldana Pires <aldana.pires@gmail.com>
2022-08-19 22:29:03 -0300

    proyecto final

--- SitioInstituto2/routes/index.js
+++ SitioInstituto2/routes/index.js
@@ -2,11 +2,34 @@ var express = require('express');
 var router = express.Router();
 var nodemailer = require('nodemailer');
 var novedadesModel = require('../models/novedadesModel');
+var cloudinary = require('cloudinary').v2;
 
 /* GET home page. */
 router.get('/', async function (req, res, next) {
 
   var novedades = await novedadesModel.getNovedades();
+  novedades = novedades.splice(0,5);
+  novedades = novedades.map(novedad => {
+    if (novedad.img_id) {
+        const imagen = cloudinary.url(novedad.img_id, {
+            width: 120,
+            height: 50,
+            crop: 'fill'
+        });
+        const imagenfull = cloudinary.url(novedad.img_id, {});
+        return {
+            ...novedad,
+            imagen,
+            imagenfull
+        }
+    } else {
+        return {
+            ...novedad,
+            imagen: '',
+            imagenfull: ''
+        }
+    }
+});
 
   res.render('index', {
     novedades
